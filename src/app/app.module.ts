966220c39d3b0cc4d2433ab88ca96e093da4485c
Adrian Lima <adrigato3@gmail.com>
2021-01-14 17:57:11 -0500

    Primer commit, Proyecto inicializado y listo para meter código

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
+//Components
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+// Routes
+import { APP_ROUTING } from './app.routes';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+
+
+const APP_ROUTES: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: '', pathMatch:'full', redirectTo:'login' },
+    { path: '**', pathMatch:'full', redirectTo:'login' },
+];
+
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
