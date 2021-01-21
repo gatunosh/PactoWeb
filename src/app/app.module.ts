90e042000d63d5478edb8ce60f721b1591fb32a0
salome-21 <65325099+salome-21@users.noreply.github.com>
2021-01-21 18:32:05 -0500

    log/users

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule} from '@angular/forms'
+import { ReactiveFormsModule, FormsModule} from '@angular/forms'
 
 //Components
 import { AppComponent } from './app.component';
@@ -24,6 +24,7 @@ import { APP_ROUTING } from './app.routes';
     BrowserModule,
     HttpClientModule,
     ReactiveFormsModule,
+    FormsModule,
     APP_ROUTING
   ],
   providers: [],
