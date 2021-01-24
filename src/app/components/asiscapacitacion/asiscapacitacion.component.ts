a26a1722887dcd7bb563ef60b139e0ac356b66df
SteveCabezas <steveobeyy@hotmail.com>
2021-01-24 14:26:51 -0500

    capacitacion y asistencia routing funcionando

---
+++ src/app/components/asiscapacitacion/asiscapacitacion.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-asiscapacitacion',
+  templateUrl: './asiscapacitacion.component.html'
+})
+export class AsiscapacitacionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
