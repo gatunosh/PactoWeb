60828d41198044a16793b70b2cc6f455b7bee1df
dianac14 <64987362+dianac14@users.noreply.github.com>
2021-01-24 17:29:54 -0500

    prod

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,10 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { ProductosModel } from '../models/productos.models';
+
 
 @Injectable({
     providedIn: 'root'
 })
 export class ProductosService{
-   //constructor(private _http: HttpClient){}
+
+    private url:string = 'https://restserver-pacto.herokuapp.com';
+
+    constructor(private _http: HttpClient){}
+
+    getProductos(){
+    }
+
+    updateProductos(){
+
+    }
+
+    deleteProductos(){
+
+    }
+
 }
