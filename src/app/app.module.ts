b5ae679c66568db419f46f6e85499c7e4e601bfc
Adrian Lima <adrigato3@gmail.com>
2021-01-26 23:43:53 -0500

    Data table implementado y funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { ProductosComponent } from './components/productos/productos.component';
 import { EntidadComponent } from './components/entidad/entidad.component';
 import { CapacitacionComponent } from './components/capacitacion/capacitacion.component';
 import { AsiscapacitacionComponent } from './components/asiscapacitacion/asiscapacitacion.component';
-
+import { DataTablesModule } from 'angular-datatables';
 // Routes
 import { AppRoutingModule } from './app-routing.module';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
@@ -36,7 +36,8 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
     ReactiveFormsModule,
     FormsModule,
     NgxPaginationModule,
-    AppRoutingModule
+    DataTablesModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
