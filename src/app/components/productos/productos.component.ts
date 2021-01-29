fbb2013bb262b10fbcae01484a84691883bfa976
dianac14 <64987362+dianac14@users.noreply.github.com>
2021-01-28 22:01:51 -0500

    productos data

--- src/app/components/productos/productos.component.ts
+++ src/app/components/productos/productos.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit, Input,Output,EventEmitter } from '@angular/core';
 import { LoginService } from '../../services/login.service';
 import { Router, ActivatedRoute } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { ProductosService } from 'src/app/services/productos.service';
 import { ProductosModel, Producto} from '../../models/productos.models';
 import { Subject } from 'rxjs';
 import { FormBuilder, FormGroup, NgForm} from '@angular/forms';
-import Swal from 'sweetalert2';
 
 
 @Component({
@@ -17,29 +16,14 @@ export class ProductosComponent implements OnInit {
 
   dtOptions: DataTables.Settings = {};
   dtTrigger: Subject<any> = new Subject<any>();
-  @Input() producto: any =null;
   productos: Producto[] = [];
-  productosForm: FormGroup;
-  producto1: ProductosModel = new ProductosModel();
-  productoUpdate: ProductosModel = new ProductosModel();
-  private url:string = 'https://restserver-pacto.herokuapp.com';
+  
 
   constructor(private _auth: LoginService, private _router: Router, private _http: HttpClient, private _productosService:ProductosService,private activerouter:ActivatedRoute,private _builder: FormBuilder) {
-    this.productosForm = this._builder.group({
-      nom_pro: ['',],
-      desc_pro: ['',],
-      uni_pro: ['',],
-      sto_pro: ['',],
-      pvp_pro: ['',],
-      fecha_ela_pro: ['',],
-      fecha_cad_pro: ['',],
-    });
 
   }
 
   ngOnInit(): void {
-    let productoid = this.activerouter.snapshot.paramMap.get('id');
-    console.log(productoid);
     this.dtOptions = {
       pagingType: 'full_numbers',
       pageLength: 5,
@@ -48,35 +32,13 @@ export class ProductosComponent implements OnInit {
       }
     };
 
-    this._productosService.getProductos().subscribe(data=>{
-      console.log(data);  
+    this._productosService.getProductos().subscribe((resp:any)=>{
+      this.productos=resp.productos;
+      console.log(this.productos);  
+
       this.dtTrigger.next();
     });
-  }
-  enviar(values){
-    this.producto1.nom_pro = values['nom_pro'];
-    this.producto1.desc_pro = values['desc_pro'];
-    this.producto1.uni_pro = values['uni_pro'];
-    this.producto1.sto_pro = values['sto_pro'];
-    this.producto1.pvp_pro = values['pvp_pro'];
-    this.producto1.fecha_ela_pro = values['fecha_ela_pro'];
-    this.producto1.fecha_cad_pro = values['fecha_cad_pro'];
-    this._productosService.addProductos(this.producto1).subscribe((resp:any) => {
-      this.productos = resp.usuarios;
-      window.location.reload()
-      
-    }, (err) => {
-      console.log(err);
-    });
-  }
-
-  onClick(producto){
-    this.producto=producto;
-  }
-
-
-
-  
+  }  
   
 }
 
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { ProductosModel, Producto} from '../models/productos.models';
 
 @Injectable({
     providedIn: 'root'
@@ -26,30 +25,7 @@ export class ProductosService{
         console.log(this.prodToken);
         
         return this.http.get(`${this.url}/producto`,{headers});
-    }
-
-    addProductos(producto1:Producto){
-        console.log(producto1.id_cat);
-        const headers = new HttpHeaders({
-            'token': this.leerToken()
-        });
-        const authData={
-            nom_pro: producto1.nom_pro,
-            desc_pro: producto1.desc_pro,
-            uni_pro: producto1.uni_pro,
-            sto_pro: producto1.sto_pro,
-            pvp_pro:producto1.pvp_pro,
-            fecha_ela_pro: producto1.fecha_ela_pro,
-            fecha_cad_pro: producto1.fecha_cad_pro
-        };
-
-        return this.http.post(`${this.url}/producto`,authData, {headers});
-    }
-
-
-
-
-    
+    }    
     
 
 }
