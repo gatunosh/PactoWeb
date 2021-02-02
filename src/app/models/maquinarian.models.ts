5e589023156695c2bcc5547df05fbe3d72f10849
JossueG98 <73142514+JossueG98@users.noreply.github.com>
2021-02-02 01:36:48 -0500

    Add files via upload
    models (maquinarian)

---
+++ src/app/models/maquinarian.models.ts
@@ -0,0 +1,13 @@
+export class MaquinarianModel{
+    id_mant: string;
+    id_soc:string;
+    tipo_maq:string;
+    est_maq:string;
+}
+export interface Maquinarian{
+    id_mant: string;
+    id_soc:string;
+    tipo_maq:string;
+    est_maq:string;
+}
+
