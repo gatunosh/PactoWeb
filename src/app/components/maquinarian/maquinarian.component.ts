66e1adbada4b6faf04f4fefaad667a98f4eafbca
JossueG98 <73142514+JossueG98@users.noreply.github.com>
2021-02-02 01:40:44 -0500

    Update maquinarian.component.ts
    actualización (maquinarian.ts)

--- src/app/components/maquinarian/maquinarian.component.ts
+++ src/app/components/maquinarian/maquinarian.component.ts
@@ -1,38 +1,71 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit,Input, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Maquinarian, MaquinarianModel } from '../../models/maquinarian.models';
+import { MaquinarianService } from '../../services/maquinarian.service';
 import { Subject } from 'rxjs';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-maquinarian',
-  templateUrl: './maquinarian.component.html',
-  styles: [
-  ]
+  templateUrl: './maquinarian.component.html'
 })
-export class MaquinarianComponent implements OnInit, OnDestroy{
+export class MaquinarianComponent implements OnInit, OnDestroy {
+  @Input() maquinarian: any =null;
   dtOptions: DataTables.Settings = {};
   dtTrigger: Subject<any> = new Subject<any>();
-  data: any;
+  maquinaria: Maquinarian[] = [];
+  maquinariasForm: FormGroup;
+  maquinaria1: MaquinarianModel = new MaquinarianModel();
+  //usuarioUpdate: UsuarioModel = new UsuarioModel();
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private _maquinarianService: MaquinarianService,
+    private _builder: FormBuilder
 
-  ngOnInit(): void {
+  ) { this.maquinariasForm = this._builder.group({
+    id_mant:[''],
+    id_soc: ['',],
+    tipo_maq: ['',],
+    est_maq: ['',],
+    
+
+  });
+
+   }
+
+   ngOnInit(): void {
+
     this.dtOptions = {
       pagingType: 'full_numbers',
-      pageLength: 5,
-      language: {
-        url: "//cdn.datatables.net/plug-ins/1.10.22/i18n/Spanish.json"
+      pageLength: 10,
+      "language": {
+        url: "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
       }
     };
-    this.http.get('http://dummy.restapiexample.com/api/v1/employees')
-    .subscribe((res:any) => {
-          console.log(res);
-          this.data = res.data;
-          this.dtTrigger.next();
-    });          
+
+    this._maquinarianService.getMaquinarian().subscribe((resp:any) => {
+      this.maquinaria = resp.maquinaria;
+      this.dtTrigger.next();
+    });
+
   }
-  
   ngOnDestroy(): void {
     this.dtTrigger.unsubscribe();
 
 }
+/*enviar(values){
+  this.reunion.id_asoc_reu = values['id_asoc_reu'];
+  this.reunion.fec_reu = values['fec_reu'];
+  this.reunion.hor_reu = values['hor_reu'];
+  this.reunion.mul_reu = values['mul_reu'];
+  this._reunionService.addReunion(this.reunion1).subscribe((resp:any) => {
+    this.reuniones = resp.reuniones;
+    window.location.reload()
+    
+  }, (err) => {
+  });
+}*/
 }
