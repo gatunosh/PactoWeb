7e73efff1511ca79694185761c4fccaa9d3693bb
Skuntar11 <76708425+Skuntar11@users.noreply.github.com>
2021-02-02 01:03:28 -0500

    Update reunion.component.ts
    Actualización(Reunión)

--- src/app/components/reunion/reunion.component.ts
+++ src/app/components/reunion/reunion.component.ts
@@ -1,36 +1,71 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit,Input, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Reuniones, ReunionesModel } from '../../models/reunion.models';
+import { ReunionService } from '../../services/reunion.service';
 import { Subject } from 'rxjs';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-reunion',
   templateUrl: './reunion.component.html'
 })
 export class ReunionComponent implements OnInit, OnDestroy {
+  @Input() reunion: any =null;
   dtOptions: DataTables.Settings = {};
   dtTrigger: Subject<any> = new Subject<any>();
-  data: any;
+  reuniones: Reuniones[] = [];
+  reunionesForm: FormGroup;
+  reunion1: ReunionesModel = new ReunionesModel();
+  //usuarioUpdate: UsuarioModel = new UsuarioModel();
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private _reunionService: ReunionService,
+    private _builder: FormBuilder
 
-  ngOnInit(): void {
+  ) { this.reunionesForm = this._builder.group({
+    id_asoc_reu:[''],
+    fec_reu: ['',],
+    hor_reu: ['',],
+    mul_reu: ['',],
+    
+  });
+
+   }
+
+   ngOnInit(): void {
+
     this.dtOptions = {
       pagingType: 'full_numbers',
-      pageLength: 5,
-      language: {
-        url: "//cdn.datatables.net/plug-ins/1.10.22/i18n/Spanish.json"
+      pageLength: 10,
+      "language": {
+        url: "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
       }
     };
-    this.http.get('http://dummy.restapiexample.com/api/v1/employees')
-    .subscribe((res:any) => {
-          console.log(res);
-          this.data = res.data;
-          this.dtTrigger.next();
-    });          
+
+    this._reunionService.getReunion().subscribe((resp:any) => {
+      this.reuniones = resp.reuniones;
+      this.dtTrigger.next();
+    });
+
   }
-  
   ngOnDestroy(): void {
     this.dtTrigger.unsubscribe();
 
 }
+enviar(values){
+  this.reunion.id_asoc_reu = values['id_asoc_reu'];
+  this.reunion.fec_reu = values['fec_reu'];
+  this.reunion.hor_reu = values['hor_reu'];
+  this.reunion.mul_reu = values['mul_reu'];
+  this._reunionService.addReunion(this.reunion1).subscribe((resp:any) => {
+    this.reuniones = resp.reuniones;
+    window.location.reload()
+    
+  }, (err) => {
+  });
 }
+}
+
