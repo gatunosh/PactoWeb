148c101299b4abcb0e2a4d22f1ab247edefddead
JossueG98 <73142514+JossueG98@users.noreply.github.com>
2021-02-02 01:37:54 -0500

    Add files via upload
    services (maquinarian)

---
+++ src/app/services/maquinarian.service.ts
@@ -0,0 +1,72 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Maquinarian } from '../models/maquinarian.models';
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class MaquinarianService {
+
+    
+
+    private url: string = 'https://restserver-pacto.herokuapp.com';
+
+    userToken: string;
+
+    constructor(private _http: HttpClient) { }
+
+    leerToken() {
+        if (localStorage.getItem('token')) {
+            this.userToken = localStorage.getItem('token')
+        }
+        return this.userToken;
+    }
+
+    getMaquinarian() {
+        const headers = new HttpHeaders({
+            'token': this.leerToken()
+        });
+        
+        return this._http.get(`${this.url}/maquinariasocio`,{headers});
+    }
+
+   /* addPedido(pedido:Pedido){
+        const headers = new HttpHeaders({
+            'token': this.leerToken()
+        });
+       
+        const authData = {
+            id_cat: pedido.id_cat,
+            nom_pro: pedido.nom_pro,
+            desc_pro: pedido.desc_pro,
+            uni_pro: pedido.uni_pro,
+            
+        };
+        return this._http.post(`${this.url}/producto`,authData, {headers});
+    }
+
+   /* updatePedido(pedido:Pedido){
+        const headers = new HttpHeaders({
+            'token': this.leerToken()
+        });
+        const authData = {
+            id_cat: pedido.id_cat,
+            nom_pro: pedido.nom_pro,
+            desc_pro: pedido.desc_pro,
+            uni_pro: pedido.uni_pro,
+            
+        };
+        return this._http.put(`${this.url}/producto/${prodcuto._id}`,authData,{headers});
+    }
+
+    deleteEntidad(entidad:Entidad){
+        const headers = new HttpHeaders({
+            'token': this.leerToken()
+        });  
+        return this._http.delete(`${this.url}/entidad/${entidad._id}`,{headers});
+    }*/
+
+
+
+}
