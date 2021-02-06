be0673019d704d0183daa4517b0e3e52bad8a8b7
David-Chack <73144908+David-Chack@users.noreply.github.com>
2021-02-05 23:39:12 -0500

    Update pedido.service.ts
    Servicio Pedido: elaborado por: Vanessa - David

--- src/app/services/pedido.service.ts
+++ src/app/services/pedido.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { PedidoModel, Pedido } from '../models/pedido.models';
+import { ProductosModel, Producto} from '../models/productos.models';
 
 
 @Injectable({
     providedIn: 'root'
 })
 export class PedidoService {
+
+    productos: Producto[] = [];
 
     pedidos: Pedido [] = [];
     private url: string = 'https://restserver-pacto.herokuapp.com';
@@ -23,11 +26,32 @@ export class PedidoService {
 
     getPedidos() {
         const headers = new HttpHeaders({
+            'token': localStorage.getItem('token')
+        });
+        //console.log(this.pedToken);
+        return this._http.get(`${this.url}/factura`,{headers});
+    }
+
+    getProductos() {
+        const headers = new HttpHeaders({
             'token': this.leerToken()
         });
-        console.log(this.pedToken);
+        //console.log(this.prodToken);
         return this._http.get(`${this.url}/producto`,{headers});
     }
+
+    addProductos(producto1:Producto){
+        console.log(producto1.nom_pro);
+        const headers = new HttpHeaders({
+            'token': this.leerToken()
+        });
+        const authData={
+            nom_pro: producto1.nom_pro,
+            desc_pro: producto1.desc_pro
+        };
+        return this._http.post(`${this.url}/producto`,authData, {headers});
+    }
+    
 
     addPedido(pedido1:Pedido){
         
@@ -36,10 +60,11 @@ export class PedidoService {
         });
         
         const authData = {
-            id_cat: pedido1.id_cat,
-            nom_pro: pedido1.nom_pro,
-            desc_pro: pedido1.desc_pro,
-            uni_pro: pedido1.uni_pro,
+            id_cli: pedido1.id_cli,
+            fec_fac: pedido1.fec_fac,
+            tot_fac: pedido1.tot_fac,
+            estado: pedido1.estado,
+            
             
         };
         return this._http.post(`${this.url}/producto`,authData, {headers});
