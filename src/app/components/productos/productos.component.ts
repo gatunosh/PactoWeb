259f0a4daf934a0e59a27813a44566ce2365c5ee
dianac14 <64987362+dianac14@users.noreply.github.com>
2021-03-19 17:45:48 -0500

    productos

--- src/app/components/productos/productos.component.ts
+++ src/app/components/productos/productos.component.ts
@@ -120,8 +120,8 @@ export class ProductosComponent implements OnDestroy, OnInit {
     //     console.log(this.categoria._id);
     // }
     this.producto1.id_cat = values['id_cat'];
-    // this.producto1.aso_ps = values['aso_ps'];
-    this.producto1.aso_ps = this.asociacion._id;
+    this.producto1.aso_ps = values['aso_ps'];
+    //this.producto1.aso_ps = this.asociacion._id;
     this.producto1.nom_pro = values['nom_pro'];
     this.producto1.desc_pro = values['desc_pro'];
     this.producto1.uni_pro = values['uni_pro'];
