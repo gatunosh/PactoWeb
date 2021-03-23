de668c9c1e237c267994184f864e1ef4f6f31cd8
David-Chack <73144908+David-Chack@users.noreply.github.com>
2021-03-22 21:35:54 -0500

    Update pedido.component.ts
    rediseño

--- src/app/components/pedido/pedido.component.ts
+++ src/app/components/pedido/pedido.component.ts
@@ -3,7 +3,10 @@ import { LoginService } from '../../services/login.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { PedidoService } from '../../services/pedido.service';
+import { ProductosService } from 'src/app/services/productos.service';
 import { ProductosModel, Producto} from '../../models/productos.models';
+import { categoriaProducto, categoriaProductoModel } from '../../models/categoria.models';
+import { Asociacion } from 'src/app/models/asociaciones.models';
 import { PedidoModel, Pedido} from '../../models/pedido.models';
 import { Subject } from 'rxjs';
 import { FormBuilder, FormGroup, NgForm, Validators} from '@angular/forms';
@@ -44,18 +47,26 @@ export class PedidoComponent implements OnInit, OnDestroy{
   pedidos: Pedido[] = [];
   productos: Producto[] = [];
   productosForm: FormGroup;
+  categorias: categoriaProducto[] = [];
+  asociaciones: Asociacion[] = [];
+  asociacion: any;
   pedidosForm: FormGroup;
   producto1: ProductosModel = new ProductosModel();
   pedido1: PedidoModel = new PedidoModel();
+  categoria: categoriaProductoModel = new categoriaProductoModel();
+  categoriaForm: FormGroup;
   usuarios: Usuario[] = [];
   usuario: UsuarioModel = new UsuarioModel();
   //pedidoUpdate: PedidoModel = new PedidoModel();
+
+  
 
   constructor(
     private _auth: LoginService,
     private _router: Router,
     private _http: HttpClient,
     private _pedidoService:PedidoService,
+    private _productosService: ProductosService,
     private activerouter:ActivatedRoute,
     private _userService: UsuarioService,
     private _builder: FormBuilder){
@@ -70,9 +81,30 @@ export class PedidoComponent implements OnInit, OnDestroy{
       nombre: ['',Validators.required],
       descripcion: ['',],
     });
+    this.categoriaForm = this._builder.group({
+      nombre: ['', Validators.required],
+      descripcion: ['',],
+    });
    } 
 
+  get errorCtrProducto() {
+    return this.productosForm.controls;
+  }
+  get errorCtrCategoria() {
+    return this.categoriaForm.controls;
+  }
+
   ngOnInit(): void {
+
+    this.productosForm = this._builder.group({
+      id_cat: ['', Validators.required],
+      aso_ps: ['', Validators.required],
+      nom_pro: ['', Validators.required],
+      desc_pro: ['',],
+      uni_pro: ['', Validators.required],
+      sto_pro: ['',],
+      pvp_pro: ['', Validators.required],
+    });
 
     this.dtOptions = {
       pagingType: 'full_numbers',
@@ -104,6 +136,29 @@ export class PedidoComponent implements OnInit, OnDestroy{
     this.dtTrigger.unsubscribe();
   }
 
+  enviarProd(values) {
+    // debugger;
+    // if(values['nom_pro']=this.categoria.nombre){
+    //     console.log(this.categoria._id);
+    // }
+    this.producto1.id_cat = values['id_cat'];
+    // this.producto1.aso_ps = values['aso_ps'];
+    this.producto1.aso_ps = this.asociacion._id;
+    this.producto1.nom_pro = values['nom_pro'];
+    this.producto1.desc_pro = values['desc_pro'];
+    this.producto1.uni_pro = values['uni_pro'];
+    this.producto1.sto_pro = values['sto_pro'];
+    this.producto1.pvp_pro = values['pvp_pro'];
+    this._productosService.addProductos(this.producto1).subscribe((resp: any) => {
+      this.productos = resp.producto1;
+      console.log(resp.productos);
+      window.location.reload()
+
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
   enviar(values){
     this.pedido1.id_cli = values['id_cli'];
     this.pedido1.fec_fac = values['fec_fac'];
@@ -124,6 +179,32 @@ export class PedidoComponent implements OnInit, OnDestroy{
 
     });
   }
+
+  enviarCategoria(values) {
+    this.categoria.nombre = values['nombre'];
+    this.categoria.descripcion = values['descripcion'];
+    this._productosService.addCategoria(this.categoria).subscribe((resp: any) => {
+      this.categorias = resp.categoria;
+      console.log(resp.categorias);
+      window.location.reload()
+
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+  val1:number;
+  val2:number;
+  rta:number;
+  
+  operacion(){
+    this.rta=this.val1*this.val2;
+  }
+
+  fecha(){
+    var f = new Date();
+    document.write(f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear());
+  }
+
+
 }
-
-
